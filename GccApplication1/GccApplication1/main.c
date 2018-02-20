283ea397bb5de8da65bbada7e50994180a7d9ab1
jeffason1997 <jeffreylantinga@hotmail.com>
2018-02-20 08:52:55 +0100

    Added the project

---
+++ GccApplication1/GccApplication1/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 20-2-2018 08:51:09
+ * Author : jeffr
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
